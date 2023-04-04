5182fff02232105e703feb2f9a0c6c9b565e0403
AlexanderShtykov <shtykovalexander@gmail.com>
2023-04-05 03:13:36 +0400

    All Done

--- index.js
+++ index.js
@@ -2,29 +2,15 @@ const inputEl = document.querySelector("input");
 const buttonEl = document.querySelector("button");
 const timerEl = document.querySelector("span");
 
-// Напишите реализацию createTimerAnimator
-// который будет анимировать timerEl
 const createTimerAnimator = () => {
   let interval;
   return (seconds) => {
     let secondPlus = 0;
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const remainingSeconds = seconds % 60;
-    const hh = hours.toString().padStart(2, "0");
-    const mm = minutes.toString().padStart(2, "0");
-    const ss = remainingSeconds.toString().padStart(2, "0");
-    const currentTime = new Date();
+    const { hh, mm, ss, currentTime } = getSecondsConverter(seconds);
     clearInterval(interval);
     interval = setInterval(() => {
       currentTime.setHours(hh, mm, ss + secondPlus);
-      innerTimeSpan =
-        currentTime.getHours() +
-        ":" +
-        currentTime.getMinutes() +
-        ":" +
-        currentTime.getSeconds();
-      timerEl.innerHTML = innerTimeSpan;
+      timerEl.innerHTML = getFormatedTime(currentTime);
       secondPlus++;
     }, 1000);
   };
@@ -43,3 +29,30 @@ buttonEl.addEventListener("click", () => {
 
   inputEl.value = "";
 });
+
+function getFormatedTime(currentTime) {
+  const addZeroToString = (currentTime) => {
+    return currentTime.toString().length == 2 ? currentTime : "0" + currentTime;
+  };
+  return (
+    addZeroToString(currentTime.getHours()) +
+    ":" +
+    addZeroToString(currentTime.getMinutes()) +
+    ":" +
+    addZeroToString(currentTime.getSeconds())
+  );
+}
+
+function getSecondsConverter(seconds) {
+  const DAY_PER_SECONDS = 86400;
+  if (seconds > DAY_PER_SECONDS) {
+    seconds = seconds - Math.floor(seconds / DAY_PER_SECONDS) * DAY_PER_SECONDS;
+  }
+
+  return {
+    hh: Math.floor(seconds / 3600),
+    mm: Math.floor((seconds % 3600) / 60),
+    ss: seconds % 60,
+    currentTime: new Date(),
+  };
+}
