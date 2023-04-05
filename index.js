c64d6a32d2272fca2e6ae1e630dd325bd2392e1b
AlexanderShtykov <shtykovalexander@gmail.com>
2023-04-05 15:25:19 +0400

    refactoring

--- index.js
+++ index.js
@@ -4,14 +4,47 @@ const timerEl = document.querySelector("span");
 
 const createTimerAnimator = () => {
   let interval;
+
+  const getFormatedTime = (currentTime) => {
+    const addZeroToString = (time) =>
+      time.toString().length == 2 ? time : "0" + time;
+
+    return (
+      addZeroToString(currentTime.getHours()) +
+      ":" +
+      addZeroToString(currentTime.getMinutes()) +
+      ":" +
+      addZeroToString(currentTime.getSeconds())
+    );
+  };
+
+  const getSecondsConverter = (seconds) => {
+    const DAY_PER_SECONDS = 86400;
+    if (seconds > DAY_PER_SECONDS) {
+      seconds =
+        seconds - Math.floor(seconds / DAY_PER_SECONDS) * DAY_PER_SECONDS;
+    }
+
+    return {
+      hh: Math.floor(seconds / 3600),
+      mm: Math.floor((seconds % 3600) / 60),
+      ss: seconds % 60,
+      currentTime: new Date(),
+    };
+  };
+
   return (seconds) => {
-    let secondPlus = 0;
+    let secondIncrement = 0;
     const { hh, mm, ss, currentTime } = getSecondsConverter(seconds);
     clearInterval(interval);
     interval = setInterval(() => {
-      currentTime.setHours(hh, mm, ss + secondPlus);
-      timerEl.innerHTML = getFormatedTime(currentTime);
-      secondPlus++;
+      currentTime.setHours(hh, mm, ss - secondIncrement);
+      spanTimer = getFormatedTime(currentTime);
+      timerEl.innerHTML = spanTimer;
+      secondIncrement++;
+      if (spanTimer === "00:00:00") {
+        clearInterval(interval);
+      }
     }, 1000);
   };
 };
@@ -29,30 +62,3 @@ buttonEl.addEventListener("click", () => {
 
   inputEl.value = "";
 });
-
-function getFormatedTime(currentTime) {
-  const addZeroToString = (currentTime) => {
-    return currentTime.toString().length == 2 ? currentTime : "0" + currentTime;
-  };
-  return (
-    addZeroToString(currentTime.getHours()) +
-    ":" +
-    addZeroToString(currentTime.getMinutes()) +
-    ":" +
-    addZeroToString(currentTime.getSeconds())
-  );
-}
-
-function getSecondsConverter(seconds) {
-  const DAY_PER_SECONDS = 86400;
-  if (seconds > DAY_PER_SECONDS) {
-    seconds = seconds - Math.floor(seconds / DAY_PER_SECONDS) * DAY_PER_SECONDS;
-  }
-
-  return {
-    hh: Math.floor(seconds / 3600),
-    mm: Math.floor((seconds % 3600) / 60),
-    ss: seconds % 60,
-    currentTime: new Date(),
-  };
-}
