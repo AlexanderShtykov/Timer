f6931a373884b4a97389b909e25ab6f9b9a6cbc9
AlexanderShtykov <shtykovalexander@gmail.com>
2023-04-16 19:21:29 +0400

    refactoring3

--- index.js
+++ index.js
@@ -4,19 +4,7 @@ const timerEl = document.querySelector("span");
 
 const createTimerAnimator = () => {
   let interval = null;
-
-  const getFormatedTime = (currentTime) => {
-    const addZeroToString = (time) =>
-      time.toString().length === 1 ? "0" + time : time;
-
-    return (
-      addZeroToString(currentTime.getHours()) +
-      ":" +
-      addZeroToString(currentTime.getMinutes()) +
-      ":" +
-      addZeroToString(currentTime.getSeconds())
-    );
-  };
+  let secondIncrement = 0;
 
   const getSecondsConverter = (seconds) => {
     const DAY_PER_SECONDS = 86400;
@@ -33,23 +21,37 @@ const createTimerAnimator = () => {
     };
   };
 
+  const getFormatedTime = (currentTime) => {
+    const addZeroToString = (time) =>
+      time.toString().length === 1 ? "0" + time : time;
+
+    return (
+      addZeroToString(currentTime.getHours()) +
+      ":" +
+      addZeroToString(currentTime.getMinutes()) +
+      ":" +
+      addZeroToString(currentTime.getSeconds())
+    );
+  };
+
+  const updateTimer = (hh, mm, ss, currentTime) => {
+    currentTime.setHours(hh, mm, ss - secondIncrement);
+
+    const timer = getFormatedTime(currentTime);
+    timerEl.textContent = timer;
+
+    secondIncrement++;
+    if (timer === "00:00:00") {
+      clearInterval(interval);
+      secondIncrement = 0;
+    }
+  };
+
   return (seconds) => {
     clearInterval(interval);
+    secondIncrement = 0;
     const { hh, mm, ss, currentTime } = getSecondsConverter(seconds);
-    let secondIncrement = 0;
-
-    const updateTimer = () => {
-      currentTime.setHours(hh, mm, ss - secondIncrement);
-      const timer = getFormatedTime(currentTime);
-      timerEl.textContent = timer;
-      secondIncrement++;
-
-      if (timer === "00:00:00") {
-        clearInterval(interval);
-      }
-    };
-
-    interval = setInterval(updateTimer, 1000);
+    interval = setInterval(() => updateTimer(hh, mm, ss, currentTime), 1000);
   };
 };
 
